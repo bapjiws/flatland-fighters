968783ea84da0b7857539dfe654aa9e5f05ba704
Alex Boklin <alex.boklin@gmail.com>
2018-04-24 00:36:28 +0200

    Tie current input to a script and show it via the Text element

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InputManager : MonoBehaviour {
+	public Text currentInput;
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +15,10 @@ public class InputManager : MonoBehaviour {
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Space)) {
 			Debug.Log("SPACE is pressed!");
-		} 
+			currentInput.text = "SPACE";
+		} else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			Debug.Log("DOWN is pressed!");
+			currentInput.text = "DOWN";
+		}
 	}
 }
